92b82de98b8b8cdb18c6ec1cd34ee04d1977c458
Zoltan <kervantas>
2019-09-22 13:21:41 +0200

    db api done

--- src/resources/db.js
+++ src/resources/db.js
@@ -4,17 +4,56 @@ const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('./src/resources/db.json')
 const db = low(adapter)
 
-//console.log(db.get('tips').value())
-
-export const newUser = (userId, userName) => {
-  const user = {
-    userId,
-    userName,
-    seen: []
-  }
+const newUser = (userId) => {
   db.get('users')
-  .push(user)
+  .push({
+    id: userId,
+    seen: []
+  })
   .write()
 }
 
-export const getUser
+const getUser = (userId) => {
+  return db.get('users')
+  .find({id: userId})
+  .value()
+}
+
+const setSeen = (userId, tipId) => {
+  db.get('users')
+  .find({id: userId})
+  .get('seen')
+  .push(tipId)
+  .write()
+}
+
+const getTipById = (tipId) => {
+  return db.get('tips')
+  .find({id: tipId})
+  .value()
+}
+
+const getNextTipForUser = (userId) => {
+  const seen = 
+  db.get('users')
+  .find({id: userId})
+  .get('seen')
+  .value()
+  const lastSeen = seen[seen.length - 1]
+  setSeen(userId, lastSeen+1)
+  return getTipById(lastSeen+1)
+}
+
+//newUser('Zoltan')
+//console.log(getTipById(1))
+//setSeen('Zoltan', 1)
+//setSeen('Zoltan', 2)
+//console.log(getNextTipForUser('Zoltan'))
+
+module.exports = {
+  newUser,
+  getUser,
+  setSeen,
+  getTipById,
+  getNextTipForUser,
+}
