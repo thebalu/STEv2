75194e1cdd0bd635206484a726b49b475b640b13
abelkocsis <kocsis.abel.98@gmail.com>
2019-10-20 22:01:06 +0200

    changes deleted

--- src/webhooks/templates.js
+++ src/webhooks/templates.js
@@ -20,25 +20,5 @@ const button = (title, postback) => {
     }
 }
 
-function firstGetUserName(user_id){
-  info = request({
-    url: "https://graph.facebook.com/v2.6/" + user_id + "?",
-    qs: {
-      access_token: config.get('facebook.access_token')
-    },
-    headers: {
-        'Accept': 'application/json',
-        'Accept-Charset': 'utf-8',
-        'User-Agent': 'test-bot'
-    },
-    method: "GET",
-    json: true,
-    time: true
-  }, 
-  function(error, res, faceUserInfo) {
-   return (error, res, faceUserInfo)
-  }
-);
-}
 
-module.exports = {button, buttonMessage, firstGetUserName}
+module.exports = {button, buttonMessage}
