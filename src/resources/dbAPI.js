0b29808a55da43c70ad02ec78d124386b73e2981
abelkocsis <kocsis.abel.98@gmail.com>
2019-10-30 02:29:57 +0100

    generateYes try13

--- src/resources/dbAPI.js
+++ src/resources/dbAPI.js
@@ -75,7 +75,7 @@ const getAllUsers = async() => {
   return users
 }
 
-const getTemplate = async (number) => {
+const getStringTemplate = async (number) => {
   return await db.collection('templateStrings').doc(String(number)).get()
     .then(x => x.data())
 }
@@ -91,5 +91,5 @@ module.exports = {
   uploadTip,
   getAllUsers,
   addUserName,
-  getTemplate
+  getStringTemplate
 }
