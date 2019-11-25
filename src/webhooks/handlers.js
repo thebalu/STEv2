ba353739eee9877070a620f16cc3630a1352bee1
abelkocsis <kocsis.abel.98@gmail.com>
2019-11-25 17:07:16 +0100

    userLan 1.22

--- src/webhooks/handlers.js
+++ src/webhooks/handlers.js
@@ -8,9 +8,9 @@ let descriptionAdded = false
 let title
 let tipDescription
 
-const handleMessage = (sender_psid, received_message) => {
+const handleMessage = async (sender_psid, received_message) => {
   console.log(received_message.text)
-  //var user = (await db.getUser(sender_psid))
+  var user = (await db.getUser(sender_psid))
   var lan = user.language;
   if (titleAdded) {
     titleAdded = false
@@ -41,7 +41,7 @@ const handleMessage = (sender_psid, received_message) => {
       break;
 
     default:
-      gernarateString("smiley", "hu_HU")
+      gernarateString("smiley", lan)
         .then(smiley => standarReply(standardReply(sender_psid, received_message, ("Szia! " + smiley + " Hogy állsz a múltkori kihívással? Mutatom mégegyszer:"))));
       break;
   }
