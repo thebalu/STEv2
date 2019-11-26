3c371248faf0b4a4892ad022e596776027e8b90e
abelkocsis <kocsis.abel.98@gmail.com>
2019-11-26 23:16:13 +0100

    laderboard 1.5

--- src/webhooks/handlers.js
+++ src/webhooks/handlers.js
@@ -391,8 +391,8 @@ const maybeShowProgress = async (sender_psid, user, done) => {
 
 const showLaderboard = async (sender_psid, user, done) => {
   let users = await db.getAllUsers()
-  users.forEach(name, done => {
-    console.log(name + ": " + done)
+  users.forEach(user_id => {
+    my_user = await db.getUser(user_id)
   });
 }
 
