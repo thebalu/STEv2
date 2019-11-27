637883a128196636a950768457085370681dd0c5
abelkocsis <kocsis.abel.98@gmail.com>
2019-11-27 17:58:14 +0100

    laderboard 2.1

--- src/webhooks/handlers.js
+++ src/webhooks/handlers.js
@@ -407,7 +407,7 @@ const showLaderboard = async (sender_psid, user, done) => {
     });
   })
 
-  fe.then(() => {
+  await fe.then(() => {
     console.log("map sort started")
     const mapSort1 = new Map([...results.entries()].sort((a, b) => b[1] - a[1]));
     console.log("mapSort1: ");             // sorted order
